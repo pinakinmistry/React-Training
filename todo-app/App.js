6812dd4aefd71773c42757b27501738f77330ead
Pinakin Mistry <pmistry@xebia.com>
2016-12-15 08:02:16 +0530

    Classy way of creating react component using ES6 class

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const App = React.createClass({
-	render: function() {
+class App extends React.Component {
+	render() {
 		return <h1>My First React Component</h1>
 	}
-})
+}
 
 export default App

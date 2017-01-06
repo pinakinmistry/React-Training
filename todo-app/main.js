2b5b5f3b3bc434fcf8a59a5db7dbcbfc3beb668a
Pinakin Mistry <pmistry@xebia.com>
2017-01-06 21:07:32 +0530

    Nested Routes and default routing using IndexRoute

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router'
 
+const Outer = (props) => <div><h1>ToDo App</h1><Links />{props.children}</div>
+const About = (props) => <div><h1>About</h1>{props.children}</div>
+const Contact = (props) => <div><h1>Contact</h1>{props.children}</div>
+const Email = () => <div><h1>Email</h1></div>
+const Links = () => (
+    <nav>
+        <Link to="/">Home | </Link>
+        <Link to="/about">About | </Link>
+        <Link to="/about/contact">Contact | </Link>
+        <Link to="/about/contact">Email</Link>
+    </nav>
+)
 ReactDOM.render(
-    <Router history={ browserHistory }>
-        <Route path="/(:show)" component={App}></Route>
+    <Router history={browserHistory}>
+        <Route path="/" component={Outer}>
+            <Route path="about" component={About}>
+                <Route path="contact" component={Contact}>
+                    <IndexRoute component={Email}></IndexRoute>
+                </Route>
+            </Route>
+        </Route>
     </Router>,
-    document.getElementById('app'))
+    document.getElementById('app')
+)
+
+// ReactDOM.render(
+//     <Router history={ browserHistory }>
+//         <Route path="/(:show)" component={App}></Route>
+//     </Router>,
+//     document.getElementById('app'))

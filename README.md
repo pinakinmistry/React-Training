1e4aab94e37102ce957de470a0b35b8fbd716f05
Pinakin Mistry <pmistry@xebia.com>
2017-01-15 12:37:20 +0530

    Testing counter reducer using expect library

--- README.md
+++ README.md
@@ -971,16 +971,16 @@ const counter = (state = 0, action) => {
 	}
 }
 
-expect(counter(0, { type: 'ÍNCREMENT' })).toEqual(1);
+expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
 
-expect(counter(1, { type: 'ÍNCREMENT' })).toEqual(2);
+expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
 
 expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
 
 expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
 
 console.log('All tests passed');
-```
+``` 
 
 
 
--- todo-app/main.js
+++ todo-app/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router'
+import expect from 'expect'
 
 const Home = React.createClass({
     componentWillMount() {
@@ -41,3 +42,25 @@ ReactDOM.render(
 //         <Route path="/(:show)" component={App}></Route>
 //     </Router>,
 //     document.getElementById('app'))
+
+
+const counter = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT':
+			return state - 1;
+		default:
+			return state;
+	}
+}
+
+expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+
+expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+
+expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+
+expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
+
+console.log('All tests passed');

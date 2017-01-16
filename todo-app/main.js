779abd215c02e810adbbdce0373df357e124544e
Pinakin Mistry <pmistry@xebia.com>
2017-01-16 07:50:48 +0530

    Implementing store from scratch

--- todo-app/main.js
+++ todo-app/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router'
 import expect from 'expect'
-import { createStore } from 'redux'
+//import { createStore } from 'redux'
 
 const Home = React.createClass({
     componentWillMount() {
@@ -54,6 +54,29 @@ const counter = (state = 0, action) => {
 		default:
 			return state;
 	}
+}
+
+const createStore = (reducer) => {
+    let state;
+    let listeners = []
+
+    const getState = () => state
+
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach(listener => listener())
+    }
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listerners = listerners.filter(l => l != listener)
+        }
+    }
+
+    dispatch({})
+
+    return { getState, dispatch, subscribe }
 }
 
 const store = createStore(counter)

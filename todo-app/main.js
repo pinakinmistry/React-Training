9bd663cbf7f287e0e113f025d06111f1855cec05
Pinakin Mistry <pmistry@xebia.com>
2017-01-21 18:14:56 +0530

    Refactoring TodoApp component to a functional component

--- todo-app/main.js
+++ todo-app/main.js
@@ -146,40 +146,33 @@ const Footer = ({visibilityFilter, onFilterClick}) => (
 
 let todoId = 0;
 
-class TodoApp extends Component {
-    render() {
-        let {todos, visibilityFilter} = this.props;
-        const visibleTodos = getVisibleTodos(todos, visibilityFilter)
-
-        return (
-            <div>
-                <AddTodo onAddClick={(text) => {
-                    store.dispatch({
-                        type: 'ADD_TODO',
-                        id: todoId++,
-                        text
-                    })
-                }} />
-                <TodoList
-                    todos={visibleTodos}
-                    onTodoClick={id => store.dispatch({
-                        type: 'TOGGLE_TODO',
-                        id
-                    })}
-                />
-                <Footer
-                    visibilityFilter={visibilityFilter} 
-                    onFilterClick={(filter) => {
-                        store.dispatch({
-                            type: 'SET_VISIBILITY_FILTER',
-                            filter
-                        })
-                    }}
-                />
-            </div>
-        )
-    }
-}
+const TodoApp = ({todos, visibilityFilter}) => (
+    <div>
+        <AddTodo onAddClick={(text) => {
+            store.dispatch({
+                type: 'ADD_TODO',
+                id: todoId++,
+                text
+            })
+        }} />
+        <TodoList
+            todos={getVisibleTodos(todos, visibilityFilter)}
+            onTodoClick={id => store.dispatch({
+                type: 'TOGGLE_TODO',
+                id
+            })}
+        />
+        <Footer
+            visibilityFilter={visibilityFilter} 
+            onFilterClick={(filter) => {
+                store.dispatch({
+                    type: 'SET_VISIBILITY_FILTER',
+                    filter
+                })
+            }}
+        />
+    </div>
+)
 
 const render = () => {
     ReactDOM.render(

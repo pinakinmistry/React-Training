c555b9ece921ff96b007e2fa75278ec9d5d6e2aa
Pinakin Mistry <pmistry@xebia.com>
2017-01-21 10:46:29 +0530

    Toggle todo

--- todo-app/main.js
+++ todo-app/main.js
@@ -72,7 +72,18 @@ class TodoApp extends Component {
                 }}>Add Tasks</button>
                 <ul>
                     {this.props.todos.map(todo => (
-                        <li key={todo.id}>{todo.text}</li>
+                        <li key={todo.id}
+                            onClick={() => {
+                                store.dispatch({
+                                    type: 'TOGGLE_TODO',
+                                    id: todo.id
+                                })
+                            }}
+                            style={{
+                                textDecoration: todo.completed ? 'line-through' : 'none'
+                            }}>
+                            {todo.text}
+                        </li>
                     ))}
                 </ul>
             </div>

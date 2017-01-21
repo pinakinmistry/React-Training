a654136a76c2b331488f9bb9c63bb68d9f365601
Pinakin Mistry <pmistry@xebia.com>
2017-01-21 12:29:28 +0530

    Readable code

--- todo-app/main.js
+++ todo-app/main.js
@@ -120,11 +120,26 @@ class TodoApp extends Component {
                 <p>
                     Show:
                     {' '}
-                    <FilterLink filter="SHOW_ALL" currentFilter={visibilityFilter}>All</FilterLink>
+                    <FilterLink
+                        filter="SHOW_ALL"
+                        currentFilter={visibilityFilter}
+                    >
+                        All
+                    </FilterLink>
                     {' '}
-                    <FilterLink filter="SHOW_ACTIVE" currentFilter={visibilityFilter}>Active</FilterLink>
+                    <FilterLink
+                        filter="SHOW_ACTIVE"
+                        currentFilter={visibilityFilter}
+                    >
+                        Active
+                    </FilterLink>
                     {' '}
-                    <FilterLink filter="SHOW_COMPLETED" currentFilter={visibilityFilter}>Completed</FilterLink>
+                    <FilterLink
+                        filter="SHOW_COMPLETED"
+                        currentFilter={visibilityFilter}
+                    >
+                        Completed
+                    </FilterLink>
                 </p>
             </div>
         )

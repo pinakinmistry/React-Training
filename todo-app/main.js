d7cb1dc4c9dcc7dd68f6bcb65509a123d45939da
Pinakin Mistry <pmistry@xebia.com>
2017-01-22 14:35:22 +0530

    Generating FilterLink using connect method

--- todo-app/main.js
+++ todo-app/main.js
@@ -114,35 +114,25 @@ const Link = ({active, children, onClick}) => {
     )
 }
 
-class FilterLink extends Component {
-    componentDidMount() {
-        const {store} = this.context
-        this.unsubscribe = store.subscribe(() => this.forceUpdate())
-    }
-    componentWillUnmount() {
-        this.unsubscribe()
-    }
-    render() {
-        const props = this.props
-        const {store} = this.context
-        const state = store.getState()
-
-        return (
-            <Link 
-                active={props.filter === state.visibilityFilter}
-                onClick={() => store.dispatch({
-                    type: 'SET_VISIBILITY_FILTER',
-                    filter: props.filter
-                })}
-            >
-                {props.children}
-            </Link>
-        )
+const mapStateToLinkProps = (state, ownProps) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
     }
 }
-FilterLink.contextTypes = {
-    store: React.PropTypes.object
+const mapDispatchToLinkProps = (dispatch, ownProps) => {
+    return {
+        onClick: () => {
+            dispatch({
+                type: 'SET_VISIBILITY_FILTER',
+                filter: ownProps.filter
+            })
+        }
+    }
 }
+const FilterLink = connect(
+    mapStateToLinkProps,
+    mapDispatchToLinkProps
+)(Link)
 
 const Footer = (props, {store}) => (
     <p>

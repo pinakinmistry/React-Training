6553b9e31c4c9eb617fe2337172986dd4e5ece81
pinakinmistry <pinakin.nmistry@gmail.com>
2017-02-26 09:59:37 +0530

    Throttling save state

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -5,15 +5,15 @@ import { Provider } from 'react-redux'
 import todoApp from './reducers'
 import TodoApp from './components/App'
 import { loadState, saveState } from './localStorage'
-
+import throttle from 'lodash/throttle'
 
 const persistedState = loadState()
 const store = createStore(todoApp, persistedState)
-store.subscribe(() => {
+store.subscribe(throttle(() => {
     saveState({
         todos: store.getState().todos
     })
-})
+}, 2000))
 
 ReactDOM.render(
     <Provider store={store}>

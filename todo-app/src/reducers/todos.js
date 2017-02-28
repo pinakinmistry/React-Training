a862a20538302b7ff6be1ccba52268088651bc31
pinakinmistry <pinakin.nmistry@gmail.com>
2017-03-01 00:52:01 +0530

    Normalizing state shape to avoid data inconsistency accross app

--- todo-app/src/reducers/todos.js
+++ todo-app/src/reducers/todos.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux'
+
 const todo = (state, action) => {
     switch(action.type) {
         case 'ADD_TODO':
@@ -19,29 +21,47 @@ const todo = (state, action) => {
     }
 }
 
-const todos = (state = [], action) => {
-    switch(action.type)  {
+const byId = (state = {}, action) => {
+    switch(action.type) {
         case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ]
         case 'TOGGLE_TODO':
-            return state.map(t => todo(t, action))
+            return {
+                ...state,
+                [action.id]: todo(state[action.id], action)
+            }
         default:
             return state
     }
 }
 
+const allIds = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return [...state, action.id]
+        default:
+            return state
+    }
+}
+
+const todos = combineReducers({
+    byId,
+    allIds,
+})
+
 export default todos
 
+const getAllTodos = (state) =>
+    state.allIds.map(id => state.byId[id])
+
 export const getVisibleTodos = (state, filter) => {
+    const allTodos = getAllTodos(state)
+
     switch(filter) {
         case 'all':
-            return state
+            return allTodos
         case 'active':
-            return state.filter(todo => !todo.completed)
+            return allTodos.filter(todo => !todo.completed)
         case 'completed':
-            return state.filter(todo => todo.completed)
+            return allTodos.filter(todo => todo.completed)
     }
 }

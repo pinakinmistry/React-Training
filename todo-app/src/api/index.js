5ed0f65a3e49dee689c553ed07859acbbf877969
pinakinmistry <pinakin.nmistry@gmail.com>
2017-03-11 11:41:36 +0530

    Fake backend

---
+++ todo-app/src/api/index.js
@@ -0,0 +1,33 @@
+import { v4 } from 'node-uuid'
+
+const fakeDatabase = {
+    todos: [{
+        id: v4(),
+        text: 'Learn React',
+        completed: true
+    }, {
+        id: v4(),
+        text: 'Learn React Router',
+        completed: true
+    }, {
+        id: v4(),
+        text: 'Learn Redux',
+        completed: false
+    }]
+}
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+export const fetchTodos = (filter) =>
+    delay(500).then(() => {
+        switch(filter) {
+            case 'all':
+                return fakeDatabase.todos
+            case 'active':
+                return fakeDatabase.todos.filter(todo => !todo.completed)
+            case 'completed':
+                return fakeDatabase.todos.filter(todo => todo.completed)
+            default:
+            throw new Error(`Unknown filter: ${filter}`)
+        }
+    })

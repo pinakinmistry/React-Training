95d12fb229a970b8f04df7086259f4b5514cb509
pinakinmistry <pinakin.nmistry@gmail.com>
2017-03-12 12:06:03 +0530

    Applying redux middlewares

--- todo-app/src/configureStore.js
+++ todo-app/src/configureStore.js
@@ -1,49 +1,22 @@
 import { loadState, saveState } from './localStorage'
 import throttle from 'lodash/throttle'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 import todoApp from './reducers'
-
-const logger = (store) => (next) => {
-    if(!console.group) {
-        return next
-    }
-
-    return (action) => {
-        console.group(action.type)
-        console.log('%c prev state', 'color: gray', store.getState())
-        console.log('%c action', 'color: blue', action)
-        const returnValue = next(action)
-        console.log('%c next state', 'color: green', store.getState())
-        console.groupEnd(action.type)
-        return returnValue
-    }
-}
-
-const promise = (store) => (next) => (action) => {
-    if(typeof action.then === 'function') {
-        return action.then(next)
-    } else {
-        next(action)
-    }
-}
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-    middlewares.slice().reverse().forEach(middleware => {
-        store.dispatch = middleware(store)(store.dispatch)
-    })
-}
 
 const configureStore = () => {
     const persistedState = loadState()
-    const store = createStore(todoApp)
     const middlewares = [promise]
 
     if(process.env.NODE_ENV !== 'production') {
-        middlewares.push(logger)
+        middlewares.push(createLogger())
     }
 
-    wrapDispatchWithMiddlewares(store, middlewares)
-
+    const store = createStore(
+        todoApp,
+        applyMiddleware(...middlewares)
+    )
     store.subscribe(throttle(() => {
         saveState({
             todos: store.getState().todos

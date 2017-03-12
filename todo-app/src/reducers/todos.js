291dd5e4db4726d699420e61e13999288259de11
pinakinmistry <pinakin.nmistry@gmail.com>
2017-03-12 17:15:45 +0530

    Updating state with fetched data

--- todo-app/src/reducers/todos.js
+++ todo-app/src/reducers/todos.js
@@ -1,67 +1,68 @@
 import { combineReducers } from 'redux'
 
-const todo = (state, action) => {
-    switch(action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            }
-        case 'TOGGLE_TODO':
-            if(state.id != action.id) {
-                return state
-            }
-            return {
-                ...state,
-                completed: !state.completed
-            }
-        default:
-            return state
-    }
-}
-
 const byId = (state = {}, action) => {
     switch(action.type) {
-        case 'ADD_TODO':
-        case 'TOGGLE_TODO':
-            return {
-                ...state,
-                [action.id]: todo(state[action.id], action)
-            }
+        case 'RECEIVE_TODOS':
+            const nextState = { ...state }
+            action.response.forEach(todo => {
+                nextState[todo.id] = todo
+            })
+            return nextState
         default:
             return state
     }
 }
 
 const allIds = (state = [], action) => {
+    if(action.filter !== 'all') {
+        return state
+    }
     switch(action.type) {
-        case 'ADD_TODO':
-            return [...state, action.id]
+        case 'RECEIVE_TODOS':
+            return action.response.map(todo => todo.id)
         default:
             return state
     }
 }
 
+const activeIds = (state = [], action) => {
+    if(action.filter !== 'active') {
+        return state
+    }
+    switch(action.type) {
+        case 'RECEIVE_TODOS':
+            return action.response.map(todo => todo.id)
+        default:
+            return state
+    }
+}
+
+const completedIds = (state = [], action) => {
+    if(action.filter !== 'completed') {
+        return state
+    }
+    switch(action.type) {
+        case 'RECEIVE_TODOS':
+            return action.response.map(todo => todo.id)
+        default:
+            return state
+    }
+}
+
+const allIdsByFilter = combineReducers({
+    all: allIds,
+    active: activeIds,
+    completed: completedIds
+})
+
 const todos = combineReducers({
     byId,
-    allIds,
+    allIdsByFilter,
 })
 
 export default todos
 
-const getAllTodos = (state) =>
-    state.allIds.map(id => state.byId[id])
-
 export const getVisibleTodos = (state, filter) => {
-    const allTodos = getAllTodos(state)
-
-    switch(filter) {
-        case 'all':
-            return allTodos
-        case 'active':
-            return allTodos.filter(todo => !todo.completed)
-        case 'completed':
-            return allTodos.filter(todo => todo.completed)
-    }
+    const ids = state.allIdsByFilter[filter]
+    return ids.map(id => state.byId[id])
 }
